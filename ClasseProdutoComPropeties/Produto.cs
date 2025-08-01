8297d6ca45a704764f0416c3f33f7b56f5d539e1
Edilson Guimarães <eddy.g.jesus@gmail.com>
2025-08-01 19:45:22 -0300

    Adicionar arquivos de projeto.

---
+++ ClasseProdutoComPropeties/Produto.cs
@@ -0,0 +1,51 @@
+﻿
+class Produto
+{
+    private double preco;
+    private int estoque;
+    public string nome { get; set; }
+    public string marca { get; set; }
+    public double Preco
+    {
+        get => preco;
+        set
+        {
+            if (value > 0)
+            {
+                preco = value;
+
+            }
+            else {
+                preco = 10;
+            }
+        }
+        
+
+    }
+
+    public int Estoque 
+    {        
+        get => estoque;
+        set {
+            if (value > 0)
+            {
+                estoque = value;
+            }
+            else 
+            {
+                estoque = 10;
+            }
+        }
+    }
+    public string InformacoesDoProduto =>
+        $"Nome:{nome}\n Marca:{marca}\n Preço:{preco}\n Quantidade em estoque:{estoque}";
+
+
+
+
+
+
+
+
+
+}
---
+++ ClasseProdutoComPropeties/Program.cs
@@ -0,0 +1,13 @@
+﻿// Objetido do ecercicio:
+//Desenvolver a classe Produto, com os atributos nome, marca, preco e estoque.
+//Além disso, garantir que o preço e o estoque do produto sejam valores positivos
+//e criar uma propriedade que mostra detalhadamente as informações do produto, para que seja usado pela equipe de vendas.
+
+
+Produto produto = new Produto();
+
+produto.nome = "Notebook Inspiron 15";
+produto.marca = "Dell";
+produto.Preco = 5000;
+produto.Estoque = 20;
+Console.WriteLine(produto.InformacoesDoProduto);
